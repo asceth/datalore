830438cd7f6aa533366a8d5a3ac04b83e18ed75d
John Long <john.long@ors.sc.gov>
2012-06-04 13:33:24 -0400

    changes

--- app/models/report.rb
+++ app/models/report.rb
@@ -1,19 +1,29 @@
 class Report < ActiveRecord::Base
-  has_many :joins
-  has_many :fields
-
+  has_many :report_metrics
+  has_many :associations, :through => :report_metrics
   has_many :templates
 
-  accepts_nested_attributes_for :joins
-  accepts_nested_attributes_for :fields
+  accepts_nested_attributes_for :report_metrics
+
+  serialize :joins
 
   def to_s
     name
   end
 
   def to_arel
-    joins.branches.inject(joins.root.first.foreign_table) do |query, join|
-      join.with(query)
+    # tree
+    root = report_metrics.initial.first.table
+
+    associations.uniq_by {|as| [as.primary_table_name, as.foreign_table_name]}.inject(root) do |query, assoc|
+      query.join(
+    end
+  end
+
+  def to_arel_recurse(query, tables)
+    tables.inject(query) do |query, table, children|
+      query.
+      to_arel_recurse(query,
     end
   end
 
--- db/migrate/20120529135848_add_reports.rb
+++ db/migrate/20120529135848_add_reports.rb
@@ -1,13 +1,22 @@
 class AddReports < ActiveRecord::Migration
   def change
-    create_table :reports do |t|
+    create_table :metrics do |t|
       t.string :name
+      t.string :category
+
+      t.boolean :initial
+
+      t.string :table_name
+      t.string :column_name
+
       t.timestamps
     end
 
-    create_table :joins do |t|
+    create_table :associations do |t|
       t.belongs_to :report
-      t.boolean :root, :default => false
+      t.belongs_to :association
+
+      t.string :name
 
       t.string :primary_table_name
       t.string :primary_key
@@ -18,17 +27,35 @@ class AddReports < ActiveRecord::Migration
       t.timestamps
     end
 
-    create_table :fields do |t|
-      t.belongs_to :report
+    create_table :reports do |t|
+      t.string :name
+      t.string :category
 
-      t.boolean :filterable, :default => false
-      t.boolean :displayable, :default => false
-      t.boolean :groupable, :default => false
+      t.text :joins
+
+      t.timestamps
+    end
+
+    create_table :report_metrics do |t|
+      t.belongs_to :report
+      t.belongs_to :metric
+      t.belongs_to :association
+
+
+      t.integer :position # columns
+
+      t.integer :metric_type # column, filter, group
+
+      # filters
+      t.boolean :locked
+
+      t.string :function
+      t.string :operator
+      t.string :value
+      # end filters
+
 
       t.string :name
-
-      t.string :table_name
-      t.string :column_name
 
       t.timestamps
     end
@@ -38,25 +65,18 @@ class AddReports < ActiveRecord::Migration
 
       t.string :name
 
+      t.string :hidden_columns
+
       t.timestamps
     end
 
-    create_table :template_fields do |t|
+    create_table :template_filters do |t|
       t.belongs_to :template
-      t.belongs_to :field
+      t.belongs_to :report_filter
 
-      t.integer :field_type
-
-      # when displaying
-      t.integer :position
-      t.string :name
-
-      # when filtering or grouping
       t.string :function
-
-      # when filtering
-      t.string :value
       t.string :operator
+      t.string :value
 
       t.timestamps
     end
